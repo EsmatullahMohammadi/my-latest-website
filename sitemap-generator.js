a4c1b6c2a343766bda90bd8aac9184e87e3f8881
Esmatullah Mohammadi <esmatullah1382@gmail.com>
2025-01-01 22:17:50 +0430

    sit mape generated

---
+++ sitemap-generator.js
@@ -0,0 +1,22 @@
+const { SitemapStream, streamToPromise } = require('sitemap');
+const fs = require('fs');
+const path = require('path');
+
+const links = [
+  { url: '/', changefreq: 'daily', priority: 1.0 }, // تنها صفحه اصلی
+];
+
+const sitemap = new SitemapStream({ hostname: 'https://www.esmatullah-mohammadi-weblog.vercel.app' });
+
+streamToPromise(sitemap)
+  .then((data) => {
+    fs.writeFileSync(
+      path.join(__dirname, 'public', 'sitemap.xml'),
+      data
+    );
+    console.log('✅ Sitemap successfully created!');
+  })
+  .catch((err) => console.error('❌ Error generating sitemap:', err));
+
+links.forEach((link) => sitemap.write(link));
+sitemap.end();
